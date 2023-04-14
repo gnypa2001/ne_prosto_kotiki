cac61ef409257e822120c1fcd1db1a04232c2f07
gnypa2001 <gnypa01@gmail.com>
2023-04-14 17:35:22 +0100

    Add product deleting

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 from product import Product
 from user import User
-from saper import field
 import json
 app = Flask("Grocery")
 
@@ -28,35 +27,20 @@ def add_product():
 @app.route('/products')
 @app.route('/products', methods=["POST"])
 def products():
-
-    # spisok = [Product("https://www.meme-arsenal.com/memes/5ba73d1f4ff36b59825c28574db0de4b.jpg", 
-    #                                                           "удивленный котик", "50 рублей"),
-    #                                                   Product("http://memchik.ru/images/mems/5d94977f21248.jpg",
-    #                                                           "котик дебошЫр", "200 уе в час"),
-    #                                                   Product("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8vD0_zkG3Fnn45L49ZwlODXgPtRxwaRHxwA&usqp=CAU",
-    #                                                           "милаха", "бесценен"),
-    #                                                   Product("https://img-fotki.yandex.ru/get/4407/79015228.136/0_9d230_33ef3c68_XL.jpg",
-    #                                                           "котик наркоман", "отдадим бесплатно в хороше руки")]
-    # products = {}
-
-    # for product in spisok:
-    #     products[product.name] = {}
-    #     products[product.name]["photo"] = product.link
-    #     products[product.name]["price"] = product.price
-
-    # print(products)
-    # with open("products.json", "w") as file:
-    #     json.dump(products, file)
-
     with open("products.json", "r") as file:
         temp = json.load(file)
     if len(request.get_data()) != 0:
-        if request.form['name'] != "" and request.form['link'] != "" and request.form['price'] != "":
+        if 'name' in request.form and request.form['name'] != "" and \
+            'link' in request.form and request.form['link'] != "" and \
+            'price' in request.form and request.form['price'] != "":
             temp[request.form['name']] = {"photo": request.form['link'], "price": request.form['price']}
             with open("products.json", "w") as file:
                 json.dump(temp, file, indent=3)
-            
-    
+        elif 'delete_name' in request.form and request.form['delete_name'] != "" :
+            temp.pop(request.form['delete_name'])
+            with open("products.json", "w") as file:
+                json.dump(temp, file, indent=3)
+                
     spisok = []
     for name in temp:
         spisok.append(Product(temp[name]['photo'], name, temp[name]['price']))
@@ -82,29 +66,29 @@ def fio(name, father, surname):
 Фамилия {surname}
 """
 
-@app.route('/saper_post')
-def saper_post():
-    return render_template ("saper_post.html")
+# @app.route('/saper_post')
+# def saper_post():
+#     return render_template ("saper_post.html")
     
 
 
 
-@app.route('/saper/<height>_<width>_<bombs>')
-@app.route('/saper', methods=["POST"])
-def saper(height = 8, width = 8, bombs = 10):
-    print(request.form['height'])
-    try:
-        if int(request.form['height']) <= 0 or int(request.form['width']) <= 0 or int(request.form['bombs']) <= 0:
-            raise ValueError("Все параметры должны быть положительными")
-        if int(request.form['bombs']) > int(request.form['height']) * int(request.form['width']):
-            raise ValueError("Количество бомб не должно быть больше чем количество клеток")
-    except ValueError as e:
-        print("Ошибка:", e)
-        return render_template("saper_post.html")
-    except TypeError as e:
-        print("Ошибка:", e)
-        return render_template("saper_post.html")
-    return render_template("saper.html", game_field = field(int(request.form['height']), int(request.form['width']), int(request.form['bombs'])))
+# @app.route('/saper/<height>_<width>_<bombs>')
+# @app.route('/saper', methods=["POST"])
+# def saper(height = 8, width = 8, bombs = 10):
+#     print(request.form['height'])
+#     try:
+#         if int(request.form['height']) <= 0 or int(request.form['width']) <= 0 or int(request.form['bombs']) <= 0:
+#             raise ValueError("Все параметры должны быть положительными")
+#         if int(request.form['bombs']) > int(request.form['height']) * int(request.form['width']):
+#             raise ValueError("Количество бомб не должно быть больше чем количество клеток")
+#     except ValueError as e:
+#         print("Ошибка:", e)
+#         return render_template("saper_post.html")
+#     except TypeError as e:
+#         print("Ошибка:", e)
+#         return render_template("saper_post.html")
+#     return render_template("saper.html", game_field = field(int(request.form['height']), int(request.form['width']), int(request.form['bombs'])))
 
 app.run(debug=True)
 
